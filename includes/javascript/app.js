a21d0a3aad8240f0f35e15c9c3b3cf7bb491dad4
shimonsw <shimi0007@hotmail.com>
2015-04-22 10:37:36 +0300

    WIP - Home Page

--- includes/javascript/app.js
+++ includes/javascript/app.js
@@ -5,9 +5,84 @@ app.config(['$routeProvider', function($routeProvider){
     $routeProvider
         .when('/home',{
             templateUrl: 'partials/home.html',
-            controller: 'main',
+            controller: 'home',
             resolve: { resolvedVal: function(){ return; }}}
-    ).otherwise({redirectTo: '/home'});
+        )
+        .when('/menu',{
+            templateUrl: 'partials/menu.html',
+            controller: 'menu',
+            resolve: { resolvedVal: function(){ return; }}}
+        )
+        .when('/cart',{
+            templateUrl: 'partials/cart.html',
+            controller: 'cart',
+            resolve: { resolvedVal: function(){ return; }}}
+         )
+        .when('/status',{
+            templateUrl: 'partials/status.html',
+            controller: 'status',
+            resolve: { resolvedVal: function(){ return; }}}
+        ).otherwise({redirectTo: '/home'});
 
 }]);
+
+app.controller('header', function($scope){
+
+    $scope.title = 'ראשי';
+    $scope.setTitle = function(title){
+        $scope.title = title;
+    }
+
+    var counter = 1;
+    $scope.slideMenu = function(){
+        //e.preventDefault();
+        var $body = $('.body');
+        var $mobile_menu_container = $('.mobile-menu-container');
+        var $container = $('.hamburger .container');
+        var transitionEnd = 'transitionend webkitTransitionEnd otransitionend MSTransitionEnd';
+
+        if(counter % 2 == 1) {
+            if($container.hasClass('rotate-left'))
+                $container.removeClass('rotate-left');
+            $container.toggleClass('rotate-right');
+            counter = 0;
+        }
+        else {
+            if(counter % 2 == 0) {
+                if($container.hasClass('rotate-right'))
+                    $container.removeClass('rotate-right');
+                $container.toggleClass('rotate-left');
+                counter = 1;
+            }
+        }
+
+        $mobile_menu_container.toggleClass('right');
+
+        $container.on(transitionEnd, function() {
+            $container.off(transitionEnd);
+        });
+
+        $mobile_menu_container.on(transitionEnd, function(){
+            $body.removeClass('right');
+            $mobile_menu_container.off(transitionEnd);
+        });
+    }
+
+});
+
+function home($scope){
+
+};
+
+function menu($scope){
+
+};
+
+function cart($scope){
+
+};
+
+function status($scope){
+
+};
 
