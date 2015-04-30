8e90359e0ed0cd6cfa251a254ca7708fe1808d1a
shimonsw <shimi0007@hotmail.com>
2015-04-30 11:20:31 +0300

    WIP - Cart

--- includes/javascript/menu.js
+++ includes/javascript/menu.js
@@ -9,6 +9,7 @@ app.controller('menu-types', ['$scope', 'message', function($scope, message){
         type: 'POST',
         url: url
     }).done(function(menu_types){
+        // Query arguments - id, name, image_name
         if(menu_types != false){
             $scope.menu_types = menu_types;
             $scope.$apply();
@@ -25,7 +26,7 @@ app.controller('menu-types', ['$scope', 'message', function($scope, message){
 
 }]);
 
-app.controller('menu-items', ['$scope', '$routeParams', 'message', function($scope, $routeParams, message){
+app.controller('menu-items', ['$scope', '$routeParams', 'message', 'cart', function($scope, $routeParams, message, cart){
 
     var id = $routeParams.menu_type_id;
     id = id.split('=');
@@ -52,9 +53,14 @@ app.controller('menu-items', ['$scope', '$routeParams', 'message', function($sco
         else message.showMessage('אירעה תקלה בהבאת התפריט, אנא נסה שוב מאוחר יותר');
     });
 
+    $scope.selected = function(menu_item){
+        cart.foodItem(menu_item);
+        cart.printFoodItem();
+    }
+
 }]);
 
-app.controller('menu-additions', ['$scope', '$routeParams', 'message', function($scope, $routeParams, message){
+app.controller('menu-additions', ['$scope', '$routeParams', 'message', 'cart', function($scope, $routeParams, message, cart){
 
     var type_id = $routeParams.menu_type_id;
     var item_id = $routeParams.menu_item_id;
@@ -73,31 +79,57 @@ app.controller('menu-additions', ['$scope', '$routeParams', 'message', function(
         // ADDITION_ITEM: addition_item_id, addition_item_name, addition_item_description, image, price
         // For each addition item we have the above information plus we have the food_item_id and the food_type_id
         if(menu_additions != false){
-
-            $scope.additions = getAdditions(menu_additions);
+            var additions = getAdditions(menu_additions);
+            $scope.additions = additions;
             $scope.$apply();
-
-            //for(var i = 0; i <  additions.length; i++){
-            //    console.log(additions[i].name);
-            //    for(var j = 0; j < additions[i].addition_items.length; j++){
-            //        console.log('  ' + additions[i].addition_items[j].name);
-            //    }
-            //}
-
-            //$scope.menu_items = menu_items;
-            //$scope.$apply();
-            //$.each( $('.food-items-container').find('.right-cont'), function(i) {
-            //    //alert(menu_items[i].image_name);
-            //    $(this).css({
-            //        "background": "url('"+base_url+'/images/'+menu_items[i].image_name+"') no-repeat",
-            //        "background-size": "contain",
-            //        "background-position": "center center"
-            //    });
-            //    $scope.menu_item_id = menu_items[i].menu_item_id;
-            //})
+            $.each( $('.menu-additions-wrapper').find('.image'), function(i) {
+                //alert(menu_items[i].image_name);
+                $(this).css({
+                    "background": "url('"+base_url+'/images/'+menu_additions[i].image+"') no-repeat",
+                    "background-size": "contain",
+                    "background-position": "center center"
+                });
+                //$scope.addition_item_id = menu_additions[i].addition_item_id;
+            });
+            cart.setAdditions(additions);
         }
         else message.showMessage('אירעה תקלה בהבאת התפריט, אנא נסה שוב מאוחר יותר');
     });
+
+    $scope.selected = function(type_id, item_id){
+        var additions = cart.getAdditions();
+        handleItemsSelections(additions,type_id,item_id, message);
+    };
+
+    $scope.approve = function(additions){
+
+        var $tot_visible = $('.menu-additions-wrapper .selected').filter(':visible');
+        var type_id;
+        var item_id;
+
+        $.each($('.additions-type-container'), function(){
+            type_id = $(this).attr('id');
+            console.log(type_id);
+        });
+
+        //$.each($tot_visible, function(){
+        //    item_id = $(this).parent().parent().attr('id');
+        //    item_id = item_id.split('-')[2];
+        //    type_id = $(this).parent().parent().parent().attr('id');
+        //    console.log('type_id: '+type_id);
+        //    console.log('item_id: '+item_id);
+        //});
+
+        //var selection_type;
+        //var selections_amount;
+        //for(var i = 0; i < additions.length; i++){
+        //    if(additions[i].id == type_id){
+        //        selection_type = additions[i].selection_type;
+        //        selections_amount = additions[i].selections_amount;
+        //        break;
+        //    }
+        //}
+    }
 
 }]);
 
@@ -143,4 +175,120 @@ function additionItem(item){
     this.description = item.addition_item_description;
     this.image = item.image;
     this.price = item.price;
+}
+
+function handleItemsSelections(additions, type_id, item_id, message){
+    var duration = 400;
+    var selection_type = '';
+    var selections_amount;
+    var $length = $('.additions-type-container#'+type_id+' .selected:visible').length;
+    var $tot_selected = $('.additions-type-container#'+type_id+' .selected');
+    for(var i = 0; i < additions.length; i++){
+        if(additions[i].id == type_id){
+            selection_type = additions[i].selection_type;
+            selections_amount = additions[i].selections_amount;
+            break;
+        }
+    }
+    var type_name = additions[i].name;
+    item_id = 'addition-item-'+item_id;
+    var $selected = $('#'+item_id).find('.selected');
+    if(selection_type == 'required_exact'){
+        required_exact_handler(duration, selections_amount, $length, $selected, $tot_selected, message, type_name);
+    }
+    if(selection_type == 'optional_max'){
+        optional_max_handler(duration, selections_amount, $length, $selected, message, type_name);
+    }
+    if(selection_type == 'required_min'){
+        required_min_handler(duration, $length, $selected);
+    }
+
+}
+
+function required_min_handler(duration, $length, $selected){
+    var clicked = true;
+    if (!$selected.is(':visible')) {
+        $selected.fadeIn(duration);
+        $length++;
+        clicked = false;
+    }
+    if (clicked && $selected.is(':visible')) {
+        $selected.fadeOut(duration);
+        $length--;
+    }
+}
+
+function optional_max_handler(duration, selections_amount, $length, $selected, message, type_name){
+    var clicked = true;
+    if($length < selections_amount){
+        if (!$selected.is(':visible')) {
+            $selected.fadeIn(duration);
+            $length++;
+        }
+        else{
+            $selected.fadeOut(duration);
+            $length--;
+        }
+        clicked = false;
+    }
+    if(clicked && $length == selections_amount){
+        if (!$selected.is(':visible')) {
+            var msg = 'באפשרותך לבחור עד ';
+            msg += selections_amount;
+            msg += ' פריטים מ - ';
+            msg += '"'+type_name+'"';
+            msg += ' באפשרותך לבטל בחירה אחרת';
+            message.showMessage(msg);
+        }
+        else{
+            $selected.fadeOut(duration);
+            $length--;
+        }
+    }
+}
+
+function required_exact_handler(duration, selections_amount, $length, $selected, $tot_selected, message, type_name){
+    var clicked = true;
+    if(selections_amount == 1) {
+        if ($length < selections_amount) {
+            if (!$selected.is(':visible')) {
+                $selected.fadeIn(duration);
+                $length++;
+            }
+        }
+        if ($length == selections_amount) {
+            if (!$selected.is(':visible')) {
+                $tot_selected.fadeOut(duration);
+                $selected.fadeIn(duration);
+            }
+        }
+    }
+    if(selections_amount > 1){
+        if ($length < selections_amount) {
+            if (!$selected.is(':visible')) {
+                $selected.fadeIn(duration);
+                $length++;
+                clicked = false;
+            }
+            if (clicked && $selected.is(':visible')) {
+                $selected.fadeOut(duration);
+                $length--;
+            }
+        }
+        if($length == selections_amount){
+            if (!$selected.is(':visible')) {
+                var msg = 'עליך לבחור בדיוק ';
+                msg += selections_amount;
+                msg += ' פריטים מ - ';
+                msg += '"'+type_name+'"';
+                msg += ' באפשרותך לבטל בחירה אחרת';
+                message.showMessage(msg);
+                clicked = false;
+            }
+            if(clicked && $selected.is(':visible')){
+                $selected.fadeOut();
+                $length--;
+            }
+        }
+    }
 }
--- includes/javascript/services.js
+++ includes/javascript/services.js
@@ -32,14 +32,14 @@ app.service('message', function(){
     this.showMessage = function(msg){
         $p.html(msg);
         $screen_message.fadeIn().delay(delay).fadeOut();
-    }
+    };
 
     this.msgCloseLightbox = function(msg){
         $p.html(msg);
         $screen_message.fadeIn().delay(delay).fadeOut(function(){
             $lightbox.fadeOut();
         });
-    }
+    };
 
     this.greetings = function(msg){
         $slider_title.html(msg);
@@ -47,14 +47,78 @@ app.service('message', function(){
 
 });
 
-// Menu
-app.service('menu', function(){
+// Cart
+app.service('cart', function(){
 
-    var menu_types;
+    //var my_cart = [];
+    //var total_price = 0;
+    var food_item = {};
+    var additions = [];
 
-    this.get_menu_types = function(){
-        return menu_types;
+    //this.resetCart = function(){
+    //    my_cart = [];
+    //};
+
+    this.setAdditions = function(adds){
+        additions = [];
+        additions = adds;
+    };
+
+    this.getAdditions = function(){
+      return additions;
+    };
+
+    this.foodItem = function(item) {
+        food_item = {};
+        food_item = new foodItem(item);
+    };
+
+    this.printFoodItem = function(){
+        console.log(food_item.id);
+        console.log(food_item.name);
+        console.log(food_item.price);
+        console.log(food_item.description);
     }
+
+    function foodItem(item){
+        this.id = item.id;
+        this.name = item.name;
+        this.description = item.description;
+        this.price = item.price;
+        this.addition_types = [];
+    }
+
+    //this.getTmpFoodItem = function(){
+    //    return tmp_food_item;
+    //};
+    //
+    //this.addFoodItem = function(new_food_item){
+    //    var food_item = new foodItem(new_food_item);
+    //    my_cart.push(food_item);
+    //};
+    //
+    //this.addAdditionType = function(name){
+    //    var addition_type = new additionType(name);
+    //    my_cart[my_cart.length-1].addition_types.push(addition_type);
+    //};
+    //
+    //this.addAdditionItem = function(new_addition_item){
+    //    var addition_item = new additionItem(new_addition_item);
+    //    var length = my_cart[my_cart.length-1].addition_types.length;
+    //    my_cart[my_cart.length-1].addition_types[length-1].push(addition_item);
+    //};
+    //
+    //function additionType(){
+    //    this.name = name;
+    //    this.items = [];
+    //}
+    //
+    //function additionItem(new_addition_item){
+    //    this.id = new_addition_item.addition_item_id;
+    //    this.name = new_addition_item.addition_item_name;
+    //    this.description = new_addition_item.description;
+    //    this.price = new_addition_item.price;
+    //}
 
 });
 
